46c613a3b6917cf42e61df3d6a430403d2be24a3
Lennis <128396922+lennis-dev@users.noreply.github.com>
2023-12-07 12:12:43 +0100

    simple motor test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include "mbed.h"
 
+PortOut motor(PortC, 0xf00);
+unsigned int motorCW[] = {0x300, 0x600, 0xc00, 0x900};
+
 // main() runs in its own thread in the OS
 int main()
 {
     while (true) {
+
+        for(char i = 0; i<sizeof(motorCW); i++){
+            motor = motorCW[i];
+            thread_sleep_for(2);
+        }
 
     }
 }
